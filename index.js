064f9f5ea574168cdf5c8f59919b978e002cfbf2
Jon López De Guereña <jon.lopezdeguerena@appfire.com>
2024-03-22 14:41:10 +0100

    refactor

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/////PDF PARSER
-// const PDF = require("pdf-parse");
-// const fs = require("fs");
-
-// let PDF_FILE = "./bcExtreme.pdf";
-// let dataBuffer = fs.readFileSync(PDF_FILE);
-// PDF(dataBuffer)
-//   .then(function (data) {
-//     fs.writeFileSync(`${PDF_FILE}.txt`, data.text, {
-//       encoding: "utf8",
-//       flag: "w",
-//     });
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
-
 const { program } = require("commander");
 const fs = require("fs");
 const readline = require("readline");
---
+++ parser.js
@@ -0,0 +1,16 @@
+///PDF PARSER
+const PDF = require("pdf-parse");
+const fs = require("fs");
+
+let PDF_FILE = "./bcExtreme.pdf";
+let dataBuffer = fs.readFileSync(PDF_FILE);
+PDF(dataBuffer)
+  .then(function (data) {
+    fs.writeFileSync(`${PDF_FILE}.txt`, data.text, {
+      encoding: "utf8",
+      flag: "w",
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
